1d51374ea5bde478cb224b0e3ed41f5e679883f6
toinhao1 <toinhao89@gmail.com>
2019-04-30 11:32:26 +0200

    Auth added to all routes

--- src/index.js
+++ src/index.js
@@ -1,99 +1,42 @@
 const express = require('express');
-require('./db/mongoose');
-const User = require('./models/user');
-const Task = require('./models/task');
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//   if (req.method === 'GET') {
+//     res.send('GET requests are disabled');
+//   } else {
+//     next();
+//   }jj
+// });
+
+// app.use((req, res, next) => {
+//   if (req.method) {
+//     res.status(503).send('Site under maintenance');
+//   }
+// });
+
 app.use(express.json());
-
-app.post('/users', async (req, res) => {
-  const user = new User(req.body);
-
-  try {
-    await user.save();
-    res.status(201).send(user);
-  } catch (err) {
-    res.status(400).send(err);
-  }
-});
-
-app.get('/users', async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
-
-app.get('/users/:id', async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send('User not found');
-    }
-    res.send(user);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
-
-app.patch('/users/:id', async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ['name', 'email', 'password', 'age'];
-  const isValid = updates.every(update => allowedUpdates.includes(update));
-
-  if (!isValid) {
-    return res.status(400).send({ error: 'Invalid updates!' });
-  }
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
-    if (!user) {
-      return res.status(404).send('User not found!');
-    }
-    res.send(user);
-  } catch (err) {
-    res.status(400).send(err);
-  }
-});
-
-app.post('/tasks', async (req, res) => {
-  const task = new Task(req.body);
-  try {
-    await task.save();
-    res.status(201).send(task);
-  } catch (err) {
-    res.status(400).send(err);
-  }
-});
-
-app.get('/tasks', async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.send(tasks);
-  } catch (err) {
-    res.status(500).send('No tasks found!');
-  }
-});
-
-app.get('/tasks/:id', async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const task = await Task.findById(_id);
-    if (!task) {
-      return res.status(404).send('Task not found!');
-    }
-    res.send(task);
-  } catch (err) {
-    Response.status(500).send(err);
-  }
-});
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
   console.log('Server is up on port ' + port);
 });
+
+// const Task = require('./models/task');
+// const User = require('./models/user');
+
+// const main = async () => {
+//   // const task = await Task.findById('5cc8078472f2370575bb2dff');
+//   // await task.populate('owner').execPopulate();
+//   // console.log(task.owner);
+//   const user = await User.findById('5cc806d55267ee0551b58eab');
+//   await user.populate('tasks').execPopulate();
+//   console.log(user.tasks);
+// };
+
+// main();
