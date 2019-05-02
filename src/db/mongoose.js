98e01e3d1986a535627296624fe99c0006f250b4
toinhao1 <toinhao89@gmail.com>
2019-05-02 14:36:21 +0200

    resolving issues

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('MongoDB Running');
+  })
+  .catch(err => {
+    console.log(err);
+  });
